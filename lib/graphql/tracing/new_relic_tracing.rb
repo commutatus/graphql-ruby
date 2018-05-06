40f9c07af89656a2b27bf2deacdca3a07c0fdeb3
Michael Kevin Victor <me@mikevictor.me>
2018-05-06 20:04:41 +0530

    Add in logging

--- lib/graphql/tracing/new_relic_tracing.rb
+++ lib/graphql/tracing/new_relic_tracing.rb
@@ -16,7 +16,6 @@ module GraphQL
 
       def platform_trace(platform_key, key, data)
         begin
-          byebug
           if key == 'execute_multiplex' && data.key?(:multiplex) && data[:multiplex].queries.count == 1
             operation_type = data[:multiplex].queries.first.selected_operation.operation_type
             operation_name = data[:multiplex].queries.first.selected_operation.selections.first.name
@@ -24,6 +23,13 @@ module GraphQL
             operation_type = "UnknownType"
             operation_name = "UnknownName"
           end
+          puts "================================================="
+          puts "KEY: #{key}"
+          puts "MULTIPLEX: #{data.key?(:multiplex)}"
+          puts "QUERY COUNT: #{data[:multiplex].queries.count}"
+          puts "OPERATION TYPE: #{operation_type}"
+          puts "OPERATION NAME: #{operation_name}"
+          puts "================================================="
           NewRelic::Agent.set_transaction_name("GraphQL/#{operation_type}.#{operation_name}")
         rescue
           puts "Issue with GraphQL Instrumentation"
