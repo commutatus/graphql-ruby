522888f4d45149cd37690fc56ddccdbc217b3d9c
Michael Kevin Victor <me@mikevictor.me>
2018-05-06 14:47:01 +0530

    remove trackers

--- lib/graphql/query.rb
+++ lib/graphql/query.rb
@@ -86,7 +86,7 @@ module GraphQL
       @operations = nil
       @validate = validate
       # TODO: remove support for global tracers
-      @tracers = schema.tracers + GraphQL::Tracing.tracers + (context ? context.fetch(:tracers, []) : [])
+      @tracers = schema.tracers + GraphQL::Tracing.tracers
       # Support `ctx[:backtrace] = true` for wrapping backtraces
       if context && context[:backtrace] && !@tracers.include?(GraphQL::Backtrace::Tracer)
         @tracers << GraphQL::Backtrace::Tracer
